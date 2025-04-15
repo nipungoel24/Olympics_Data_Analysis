d7c3231eecd0e641948982cbae8daa664e5e7642
nipungoel24 <nipungoel346@gmail.com>
2025-04-15 22:57:26 +0530

    Further stuff

--- olympics-analysis-web-app/app.py
+++ olympics-analysis-web-app/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd 
+import preprocessor
+
+df = preprocessor.preprocess()
 
 st.sidebar.radio(
     'Select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athelete wise Analysis')) 
+
+st.dataframe(df)
---
+++ olympics-analysis-web-app/preprocessor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+def preprocess():
+    global df,region_df
+    
+    # Filter the dataset for Summer Olympics only
+    df = df[df['Season'] == 'Summer']
+    
+    # Merge the athlete events data with the region data
+    df = df.merge(region_df, how='left', on='NOC')
+    
+    # Drop duplicates
+    df = df.drop_duplicates()
+    
+    # One hot encoding medals 
+    df = pd.concat([df,pd.get_dummies(df['Medal'])], axis=1)
+    return df 
